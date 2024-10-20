529db0d3ec32a14155aa1b4fef4c3f8a2aa9267a
clshu <clshu2020@gmail.com>
2024-10-20 05:52:51 -0700

    Check in constants.py

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+IMDB_TOP_1000_URL = "https://www.imdb.com/search/title/?groups=top_1000"
+CHROMA_COLLECTION_NAME = "movie_collection"
+EMBEDDING_MODEL_NAME = "text-embedding-3-small"
+LLM_MODEL_NAME = "gpt-4o-mini-2024-07-18"
+UPDATE_CHROMA_DB = True
